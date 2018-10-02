c588112226c3edf6fa8cda1b47733ab59df22315
daesung <aplusasset80009335@gmail.com>
2018-10-02 17:32:22 +0900

    mainpage - subpage 연결

--- src/main/java/d2jy/admin/mainpage/controller/AdminMainPageController.java
+++ src/main/java/d2jy/admin/mainpage/controller/AdminMainPageController.java
@@ -1,8 +1,15 @@
 package d2jy.admin.mainpage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminMainPageController {
+	
+	@RequestMapping("/StudentSubPage")
+	public String studentMainPage()
+	{
+		return "subpage/student/student_sub_page";
+	}
 
 }
